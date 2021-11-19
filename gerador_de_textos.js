cac82c9e3ed96782c0e3e2f969c3e3aa0fba5c9a
Maxok System <42083051+alexisjunkes@users.noreply.github.com>
2021-11-19 13:58:52 -0300

    att

--- gerador_de_textos.js
+++ gerador_de_textos.js
@@ -37,7 +37,7 @@ for (let i = 0; i < 8; i++) {
     console.log("add")
     if(i == 0){
         newDiv.classList.add("artigo_primario")
-        minimo_largura = 3
+        minimo_largura = 4
         minimo_altura = 5
     }
     else if ((i >= 1) && (i < 4)) {
@@ -61,7 +61,7 @@ for (let i = 0; i < 8; i++) {
 <div class="moldura_do_artigo";>
         <!--<h2 class="titulo_de_artigo">${i}Titulo artigo</h2>-->
         <div class="Filtro_de_capa_De_artigo">
-        <iframe class="caixa_de_exibicao_de_html" src="Akai_niwa.html" scrolling="no"  frameborder="0" ></iframe>
+        <iframe class="caixa_de_exibicao_de_html" src="Akai_niwaamostra.html" scrolling="no"  frameborder="0" ></iframe>
         </div>
         
         <div class="div_saber_mais_livro">
@@ -69,7 +69,7 @@ for (let i = 0; i < 8; i++) {
         </div>
         
         <div class="div_saber_mais">
-            <a class="saber_mais" href="#">Mais</a> 
+            <a class="saber_mais" href="Akai_niwaleitura.html">Ler</a> 
         </div>
 
 </div>
