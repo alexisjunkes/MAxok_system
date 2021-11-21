f1957ef20d7d43fabd3d24b07ded979cefc751b4
Maxok System <42083051+alexisjunkes@users.noreply.github.com>
2021-11-21 19:49:59 -0300

    3

--- carregar_list_de_capitulos.js
+++ carregar_list_de_capitulos.js
@@ -5,6 +5,30 @@ import  {lista_de_variaveis_de_dicionario}  from "./listas_de_conteudo.js"
 
 import  {lista_de_id_adicionadis_matriz}  from "./listas_de_conteudo.js"
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var lista_de_id_adicionadis = lista_de_id_adicionadis_matriz
 
 
@@ -15,36 +39,43 @@ console.log(id_do_livro)
 var numero_de_capitulos = Object.keys(lista_de_variaveis_de_dicionario[id_do_livro]).length
 //console.log(Object.keys(lista_de_livros_para_diretorio).length)
 
+
+var main___ = document.querySelector('#corpo_principal_tela_inicial')
+
+
+/*
+const newDiv_detalhes = document.createElement("div")
+newDiv_detalhes.classList.add("detalhes_do_livro")
+newDiv_detalhes.innerHTML = `
+<div class="div_detalhes_historia_lista_De_capitulos">
+    <div class="div_detalhes_historia_lista_De_capitulos_foto">
+        <img class='imagem_de_capa_lista_de_historias_capa' src="../Capa_Livro_${id_do_livro}.png" alt="">
+        <p class="detalhes_historia_lista_De_capitulos_titulo">${lista_de_livros[id_do_livro]}</p>
+        <p class="detalhes_historia_lista_De_capitulos_detalhes">is simply dummy text of the printing and typesetting inlarised in the 1960s with theot only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+        <div class="autor_e_palavras">
+            <p class="detalhes_historia_lista_De_capitulos_autor">A. R. Junkes</p>
+            <p class="P_numero_de_palavras">Valor Não encontrado</p>
+        </div>
+        </img>
+    </div>
+    <!--<div class="div_detalhes_historia_lista_De_capitulos_titulo_e_outros_dados">
+    </div>-->
+    <div class="quadro_de_capitulos">
+    </div>
+</div>`
+main___.append(newDiv_detalhes)
+*/
+
+
+
 var dddd = 0
 console.log(lista_de_variaveis_de_dicionario)
 
 var addd = document.querySelector('.quadro_de_capitulos')
 var caixa_De_foto = document.querySelector('.quadro_de_capitulos')
-addd.innerHTML = `
-
-<div class="div_detalhes_historia_lista_De_capitulos">
-    
-    <div class="div_detalhes_historia_lista_De_capitulos_foto">
-        <img class='imagem_de_capa_lista_de_historias_capa' src="../Capa_Livro_${id_do_livro}.png" alt="">
-        <p class="detalhes_historia_lista_De_capitulos_titulo">${lista_de_livros[id_do_livro]}</p>
-
-        <p class="detalhes_historia_lista_De_capitulos_detalhes">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with theot only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with theot only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with theot only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-
-        <p class="detalhes_historia_lista_De_capitulos_autor">A. R. Junkes</p>
-        </img>
-    </div>
-    <!--<div class="div_detalhes_historia_lista_De_capitulos_titulo_e_outros_dados">
-    </div>-->
-
-</div>
-
-
-
-
-
-`
+//main___.append(addd)
 for (let i = 0; i < numero_de_capitulos; i++) {
-    //console.log(`${i} ${lista_de_livros[i]}`)
+    console.log(`${i} ${lista_de_livros[i]}`)
 
 
 
@@ -79,9 +110,13 @@ for (let i = 0; i < numero_de_capitulos; i++) {
 
 `
     addd.append(newDiv)
+ 
+   
     
 }  
 //${lista_de_variaveis_de_dicionario[i]}
 //marcador_de_id.innerHTML=marcador_de_id.innerHTML
 
-
+var numero_de_palavra = document.querySelector(".P_numero_de_palavras")
+var Get_numero = Number.parseInt(document.querySelector(".numero_de_palavras_do_livro").innerText)
+numero_de_palavra.innerText=`Número de palavras≅ ${Get_numero.toLocaleString('pt-BR')} | ${Number.parseInt(Get_numero/400)} pag.`
